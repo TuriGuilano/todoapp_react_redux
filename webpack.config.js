c9b6233aa65496c43c4ea2999c03205e19fb9eb3
Janno Kapritsias <j.kapritsias@youngcapital.nl>
2017-12-11 11:46:26 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+// let webpack know the entry point (src/app.js) and the output file
+const path = require('path');
+// find out what the exact path to this folder is from te root user
+console.log(path.join(__dirname, 'public'));
+// setup of our configurations
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  modules: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/
+    }]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentbase: path.join(__dirname, 'public')
+  }
+};
