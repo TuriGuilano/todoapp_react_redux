b620e72455a3d0fdcf36cee202d06bb402f4abe7
Ioannis Kapritsias <jkapritsias@gmail.com>
2017-12-11 17:40:46 +0100

    added scss loader

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import validator from 'validator';
 import TodoApp from '../scene/TodoApp/TodoApp';
+import './styles/style.scss';
 
 ReactDOM.render((
   <TodoApp />
