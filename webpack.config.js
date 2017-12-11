e39bbfc88363b872e320d9edb94884c8ce668406
Ioannis Kapritsias <jkapritsias@gmail.com>
2017-12-11 17:12:55 +0100

    css webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -10,10 +10,15 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
+    // rule for every js file, if js file is found run babel on it with exception of the node_modules folder
+    // rule for css files, use css-loader to inject our css in our head and style-loader to convert css into js css
+    // if you have to use multiple loaders you specify an array under the name use: [ ...loader1, loader2 ]
     rules: [{
       loader: 'babel-loader',
       test: /\.js$/,
       exclude: /node_modules/
+    }, {
+      test: /\.css$/,
     }]
   },
   devtool: 'cheap-module-eval-source-map',
